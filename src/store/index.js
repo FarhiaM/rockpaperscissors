f52a12f8f68fc745439c688a0c05374d52c21f0b
Farhia <farhia.fcb@gmail.com>
2019-12-07 15:30:56 +0100

    Implement vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,39 +4,56 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        choices: ['rock', 'paper', 'scissors'],
-        computerChoice: '',
-        userChoice: '',
-        isUserWinner: false,
-        showResult: false,
+  state: {
+    choices: ['rock', 'paper', 'scissors'],
+    computerChoice: '',
+    userChoice: '',
+    isUserWinner: false,
+    showResult: false,
+  },
+  mutations: {
+    setUserChoice(state, userChoice) {
+      // console.log('Before mutation happens:', state.userChoice);
+      state.userChoice = userChoice;
+      // console.log('Before mutation happens:', state.userChoice);
     },
-    mutations: {
-        setIsUserWinner(state, choices) {
-            let isUserWinner;
-            if (choices.userChoice === 'rock' && choices.computerChoice === 'scissor') {
-                isUserWinner = true;
-            } else if (choices.userChoice === 'paper' && choices.computerChoice === 'rock') {
-                isUserWinner = true;
-            } else if (choices.userChoice === 'scissors' && choices.computerChoice === 'paper') {
-                isUserWinner = true;
-            } else if (choices.userChoice === choices.computerChoice) {
-                isUserWinner = false;
-            } else {
-                isUserWinner = false;
-            }
-            state.isUserWinner = isUserWinner;
-        },
+    setComputerChoice(state, computerChoice) {
+      state.computerChoice = computerChoice;
     },
-    actions: {
-        playGame({ commit, state }, userSelection) {
-            const computerChoice = state.choices[Math.floor(Math.random() * state.choices.length)];
-            const userChoice = userSelection;
-            console.log(userChoice, computerChoice, state.isUserWinner);
-            commit('setIsUserWinner', { computerChoice, userChoice });
-            // this.checkWinner();
-            // this.showResult = !this.showResult;
-        },
+    setIsUserWinner(state, choices) {
+      let isUserWinner;
+      if (choices.userChoice === 'rock' && choices.computerChoice === 'scissor') {
+        isUserWinner = true;
+      } else if (choices.userChoice === 'paper' && choices.computerChoice === 'rock') {
+        isUserWinner = true;
+      } else if (choices.userChoice === 'scissors' && choices.computerChoice === 'paper') {
+        isUserWinner = true;
+      } else if (choices.userChoice === choices.computerChoice) {
+        isUserWinner = false;
+      } else {
+        isUserWinner = false;
+      }
+      state.isUserWinner = isUserWinner;
     },
-    modules: {},
+    setResult(state, bool) {
+      console.log('Before mutation happens:', state.showResult);
+      state.showResult = bool; // ! is invertig value
+      console.log('Before mutation happens:', state.showResult);
+    },
+  },
+  actions: {
+    playGame({ commit, state }, userSelection) {
+      commit('setResult', false);
+      const computerChoice = state.choices[Math.floor(Math.random() * state.choices.length)];
+      const userChoice = userSelection;
+
+      console.log(userChoice, computerChoice, state.isUserWinner);
+
+      commit('setUserChoice', userChoice);
+      commit('setComputerChoice', computerChoice);
+      commit('setIsUserWinner', { computerChoice, userChoice });
+      commit('setResult', true);
+    },
+  },
+  modules: {},
 });
