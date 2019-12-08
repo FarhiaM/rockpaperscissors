0703009fa6100707377c9fe29dca5d64a14f5da1
Farhia <farhia.fcb@gmail.com>
2019-12-08 12:42:45 +0100

    changes vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 // https://farhiam.github.io/rockpaperscissors/
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '<rockpaperscissors>'
-    : '/',
+  publicPath: '/rockpaperscissors/',
 };
